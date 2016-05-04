548793511faaa16bdc1d11fa127f231e26ca07bf
jolson6 <jolson6@nd.edu>
2016-05-04 15:35:06 -0400

    Adding the Skill model and admin code.

--- skillsmatrix/admin.py
+++ skillsmatrix/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from skillsmatrix.models import Skill
 
-# Register your models here.
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'difficulty', 'family', )
+    list_filter = ('difficulty', 'family', )
+
+admin.site.register(Skill, SkillAdmin)
--- skillsmatrix/models.py
+++ skillsmatrix/models.py
@@ -4,8 +4,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
-# Create your models here.
 
+class Skill(models.Model):
+    name = models.CharField(max_length=512)
+    difficulty = models.IntegerField()
+    family = models.CharField(max_length=256)
 
-
-
+    def __unicode__(self):
+        return self.name
