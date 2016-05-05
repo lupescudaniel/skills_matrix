517588d414e856734922404f65f55d76fe73dd7f
creinkin <creinkin@users.noreply.github.com>
2016-05-04 21:10:01 -0400

    adding commented block areas
    to help avoid merge conflicts

--- skillsmatrix/admin.py
+++ skillsmatrix/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from skillsmatrix.models import Skill, Developer
 
-
+# Skill admin configuration
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('name', 'difficulty', 'family', )
     list_filter = ('difficulty', 'family', )
 
 admin.site.register(Skill, SkillAdmin)
 
+# Developer admin configuration
 admin.site.register(Developer)
+
+# DeveloperSkills admin configuration
+
+
+# ExtraCredit admin configuration
