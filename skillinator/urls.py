29980d2371b7cfaa28832e342d073b31dc0100e8
Charles Williams <cwilli34@nd.edu>
2016-05-05 14:29:09 -0400

    DeveloperSkill fix

--- skillinator/urls.py
+++ skillinator/urls.py
@@ -16,14 +16,16 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+import skillsmatrix.views
+import skillsmatrix.homework
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     # For tutorial
-    url(r'^myskills/', 'skillsmatrix.views.MySkills'),
-    url(r'^homepage/', 'skillsmatrix.views.HomePage'),
+    url(r'^myskills/', skillsmatrix.views.MySkills),
+    url(r'^homepage/', skillsmatrix.views.HomePage),
 
     # Give access to some of the views for the testing/code coverage homework
-    url(r'^problemthree/', 'skillsmatrix.homework.ProblemThree'),
+    url(r'^problemthree/', skillsmatrix.homework.ProblemThree),
 ]
