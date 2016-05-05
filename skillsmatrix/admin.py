9ee5edf0adc4e91c50733f2f27160e6b8ca7064f
Charles Williams <cwilli34@nd.edu>
2016-05-04 21:09:39 -0400

    Mid changes for DeveloperSkill models - not yet finished

--- skillsmatrix/admin.py
+++ skillsmatrix/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from skillsmatrix.models import Skill, Developer
+from skillsmatrix.models import Skill, Developer, DeveloperSkill
 
 
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('name', 'difficulty', 'family', )
     list_filter = ('difficulty', 'family', )
 
+class DeveloperSkillAdmin(admin.ModelAdmin):
+    list_display = ('developer', 'skill', 'proficiency', 'years_of_experience')
+    list_filter = ('skill', 'proficiency', 'years_of_experience')
+
+    def get_developer_name(self, obj):
+        return obj.developer.
+
 admin.site.register(Skill, SkillAdmin)
 
 admin.site.register(Developer)
+
+admin.site.register(DeveloperSkill, DeveloperSkillAdmin)
--- skillsmatrix/models.py
+++ skillsmatrix/models.py
@@ -13,6 +13,7 @@ class Skill(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class Developer(models.Model):
     user = models.OneToOneField(User)
     extra_credit_tokens = models.IntegerField
@@ -21,3 +22,13 @@ class Developer(models.Model):
 
     def __str__(self):
         return '%s %s' %(self.user.first_name, self.user.last_name)
+
+
+class DeveloperSkill(models.Model):
+    developer = models.ForeignKey('Developer', on_delete=models.CASCADE)
+    skill = models.ForeignKey('Skill', on_delete=models.CASCADE)
+    proficiency = models.CharField(max_length=30)
+    years_of_experience = models.IntegerField()
+
+    def __unicode__(self):
+        return u'%s %s' % (self.developer.user.first_name + ' ' + self.developer.user.last_name, self.skill)
