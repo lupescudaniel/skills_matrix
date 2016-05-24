2d5da7d0d721c8a36c2538ff5605a5b233ea1670
Caleb Reinking <creinkin@nd.edu>
2016-05-23 21:16:42 -0400

    Fleshed out views for the assignment

--- skillsmatrix/views/developer.py
+++ skillsmatrix/views/developer.py
@@ -1,17 +1,26 @@
 # View file for developer pages
 
 from django.views.generic import *
+from skillsmatrix.models import *
 
 class DeveloperList(ListView):
-    pass
+    model = Developer
+    template_name = "developer_list.html"
 
 
 class DeveloperListByManager(DeveloperList):
-    pass
+    def get_queryset(self):
+        return super(DeveloperListByManager, self).get_queryset().filter(manager=self.kwargs['manager'])
 
 
 class DeveloperDetail(DetailView):
-    pass
+    model = Developer
+    template_name = "developer_detail.html"
+
+    def get_context_data(self, **kwargs):
+        c = super(DeveloperDetail, self).get_context_data()
+        c['skills'] = DeveloperSkill.objects.filter(developer=self.object).order_by('-years_of_experience')
+        return c
 
 
 
--- skillsmatrix/views/home.py
+++ skillsmatrix/views/home.py
@@ -1,5 +1,15 @@
 from django.views.generic import *
+from skillsmatrix.models import *
 
 #View file for the homepage
 class Home(TemplateView):
-    pass
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data()
+
+        context['developer_count'] = Developer.objects.count()
+        context['developerskills_count'] = DeveloperSkill.objects.count()
+        context['extracredit_count'] = ExtraCredit.objects.count()
+
+        return context
